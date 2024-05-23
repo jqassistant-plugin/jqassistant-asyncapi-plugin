98b5185c4a0de7f2fd67206df079b774a195d6f7
Maria Brauer <maria.brauer@aksa-gmbh.de>
2024-05-23 16:54:38 +0200

    corrected AsyncApiIT

--- src/test/java/org/jqassistant/plugin/asyncapi/concept/AsyncApiIT.java
+++ src/test/java/org/jqassistant/plugin/asyncapi/concept/AsyncApiIT.java
@@ -1,29 +1,28 @@
 package org.jqassistant.plugin.asyncapi.concept;
 
+import com.buschmais.jqassistant.core.report.api.model.Column;
 import com.buschmais.jqassistant.core.report.api.model.Result;
 import com.buschmais.jqassistant.core.rule.api.model.Concept;
 import com.buschmais.jqassistant.core.rule.api.model.RuleException;
 import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
 import org.jqassistant.plugin.asyncapi.api.AsyncApiScope;
-import org.jqassistant.plugin.asyncapi.api.model.ContractDescriptor;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AsyncApiIT extends AbstractPluginIT {
 
-    @BeforeEach
-    public void init() {
-        File file = new File(getClassesDirectory(AsyncApiIT.class), "testAsyncApi/channelsTest.yaml");
-        ContractDescriptor contract = getScanner().scan(file, "testAsyncApi/channelsTest.yaml", AsyncApiScope.CONTRACT);
-    }
-
     @Test
     void resolveReferences() throws RuleException {
+        File file = new File(getClassesDirectory(AsyncApiIT.class), "testAsyncApi/channelsTest.yml");
+        getScanner().scan(file, "testAsyncApi/channelsTest.yml", AsyncApiScope.CONTRACT);
         Result<Concept> result = applyConcept("asyncapi:References");
-        assertThat(result.getRows().size()).isEqualTo(1);
+        assertThat(result.getStatus()).isEqualTo(SUCCESS);
+        Column<?> references = result.getRows().get(0).getColumns().get("References");
+        assertThat(references).isNotNull();
+        assertThat(references.getValue()).isEqualTo(1L);
     }
 }
